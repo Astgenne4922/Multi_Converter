a3cafed50e868428a9d8bd6b789f42d1aa14e087
Fornari-Simone <65810491+Fornari-Simone@users.noreply.github.com>
2020-10-12 13:04:31 +0200

    Unit.js

---
+++ js/Unit.js
@@ -0,0 +1,129 @@
+const valutes = {
+    AUD	: "Australian dollar" ,	
+    BGN	: "Bulgarian lev" ,
+    BRL	: "Brazilian real" ,	
+    CAD	: "Canadian dollar"	,
+    CHF	: "Swiss franc",
+    CNY	: "Chinese yuan renminbi",
+    CZK	: "Czech koruna",
+    DKK	: "Danish krone",
+    EUR : "Euro",
+    GBP	: "Pound sterling",
+    HKD	: "Hong Kong dollar",
+    HRK	: "Croatian kuna",
+    HUF	: "Hungarian forint",
+    IDR	: "Indonesian rupiah",
+    ILS	: "Israeli shekel",
+    INR	: "Indian rupee",
+    ISK	: "Icelandic krona",
+    JPY	: "Japanese yen",
+    KRW	: "South Korean won",	
+    MXN	: "Mexican peso",	
+    MYR	: "Malaysian ringgit",
+    NOK	: "Norwegian krone",	
+    NZD	: "New Zealand dollar",
+    PHP	: "Philippine peso",
+    PLN	: "Polish zloty",
+    RON	: "Romanian leu",
+    RUB	: "Russian rouble",
+    SEK	: "Swedish krona",
+    SGD	: "Singapore dollar",
+    THB	: "Thai baht",
+    TRY	: "Turkish lira",
+    USD	: "US dollar",
+    ZAR	: "South African rand"
+};
+
+const data = {
+    BIN : "Binary",
+    DEC : "Decimal",
+    HEX : "Exadecimal"
+};
+
+const length = {
+    KM : "Kilometers",
+    MT : "Meter",
+    CM : "Centimeter",
+    MM : "Millimeter",
+    UM : "Micrometer",
+    NM : "Nanometer",
+    ML : "Mile",
+    YD : "Yard",
+    FT : "Foot",
+    IN : "Inch",
+    LY : "Light Year",
+};
+
+const temperature = {
+    CEL : "Celsius",
+    FAH : "Fahrenheit",
+    KEL : "Kelvin"
+};
+
+const weight = {
+    MG  : "Milligrams",
+    GR  : "Grams",
+    KG  : "Kilograms",
+    OUN : "Ounce",
+    POD : "Pound",
+    MT  : "Metric Ton",
+    LT  : "Long Ton",
+    ST  : "Short Ton",
+    CAR : "Karrat",
+    AMU : "Atomic Mass Unit"
+}
+
+const volume = {
+    LIT  : "Liter",
+    KM3  : "Kilometers",
+    MT3  : "Meter",
+    CM3  : "Cubic Centimeter",
+    MM3  : "Cubic Millimeter",
+    USG  : "American Gallon",
+    USQ  : "American Quart",
+    USP  : "American Pint",
+    USC  : "American Cup",
+    USO  : "American Fluid Ounce",
+    USTa : "American Table Spoon",
+    USTe : "American Tea Spoon",
+    IMG  : "Imperial Gallon",
+    IMQ  : "Imperial Quart",
+    IMP  : "Imperial Pint",
+    IMC  : "Imperial Cup",
+    IMO  : "Imperial Fluid Ounce",
+    IMTa : "Imperial Table Spoon",
+    IMTe : "Imperial Tea Spoon",
+    ML3  : "Cubic Mile",
+    YD3  : "Cubic Yard",
+    FT3  : "Cubic Foot",
+    IN3  : "Cubic Inch",
+}
+
+const area = {
+    KM2 : "Square Kilometers",
+    MT2 : "Square Meter",
+    CM2 : "Square Centimeter",
+    MM2 : "Square Millimeter",
+    UM2 : "Square Micrometer",
+    HT  : "Hectar",
+    ML2 : "Square Mile",
+    YD2 : "Square Yard",
+    FT2 : "Square Foot",
+    IN2 : "Square Inch",
+    AC  : "Acre"
+}
+
+const time = {
+    SC  : "Second",
+    MSC : "Millisecond",
+    USC : "Microsecond",
+    NSC : "Nanosecond",
+    PSC : "Picosecond",
+    MIN : "Minute",
+    HR  : "Hour",
+    DAY : "Day",
+    WK  : "Week",
+    MTH : "Month",
+    YR  : "Year"
+}
+
---
+++ js/index.js
@@ -0,0 +1,4 @@
+let vals = ["Decilitro", "Centilitro", "Millilitro", "Ettogrammo", "Grammo", "Kilometri Orari",
+            "Miglia Orarie", "Decimale", "Binario", "Esadecimale"];
+
+console.log(vals)
