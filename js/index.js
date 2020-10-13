e5e410ba94402e9bd0009cba311c9695ab14c7d2
Fornari-Simone <65810491+Fornari-Simone@users.noreply.github.com>
2020-10-13 21:24:57 +0200

    create a sort of index

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
-let vals = ["Decilitro", "Centilitro", "Millilitro", "Ettogrammo", "Grammo", "Kilometri Orari",
-            "Miglia Orarie", "Decimale", "Binario", "Esadecimale"];
-
-console.log(vals)
+let options = [ "Values", "Data"  , "Lenght", "Temperature", 
+                "Weight", "Volume", "Area"  , "Time"];
+let createDiv = () => {
+    options.forEach(element => {
+        document.body.innerHTML(
+            `<div id="${element}">
+            <h2>${element}</h2>
+            </div>`
+        )
+    });
+}
