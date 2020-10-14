ee8dd0cf8f2e8b86e762a3b0763bb7449aa9d3d9
Fornari-Simone <65810491+Fornari-Simone@users.noreply.github.com>
2020-10-14 20:22:28 +0200

    create html

--- js/index.js
+++ js/index.js
@@ -2,8 +2,21 @@ let options = [ "Values", "Data"  , "Lenght", "Temperature",
                 "Weight", "Volume", "Area"  , "Time"];
 let innHtml = `<h1>Coverter of Candido and Fornari</h1>\n`;
 options.forEach(element => {
-        innHtml += `<div id="${element}">\n
-        <h2>${element}</h2>\n
-        </div>\n`
+	innHtml += `<div id="${element}">\n
+		<h2>${element}</h2>\n
+		
+		<div>\n
+			<label class="inp" for="${element}">Importo</label>\n
+			<input type="text" name="" class="inp" id="${element}">\n
+		</div>\n
+		<div>\n
+			<label class="inp" for="${element}">Da</label>\n
+			<input type="text" name="" class="inp" id="${element}">\n
+		</div>\n
+		<div>\n
+			<label class="inp" for="${element}">A</label>\n
+			<input type="text" name="" class="inp" id="${element}">\n
+		</div>\n
+	</div>\n`
 });
 document.getElementById("body").innerHTML = innHtml;
