7ec6fa3197e5e822df60c5d8d4962412564325d4
Fornari-Simone <65810491+Fornari-Simone@users.noreply.github.com>
2020-10-14 15:49:37 +0200

    creation of Div's

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 let options = [ "Values", "Data"  , "Lenght", "Temperature", 
                 "Weight", "Volume", "Area"  , "Time"];
-
 let innHtml = `<h1>Coverter of Candido and Fornari</h1>\n`;
 options.forEach(element => {
         innHtml += `<div id="${element}">\n
