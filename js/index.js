702ce5aa1e068fc2e66f91070c20e277a0aa22f8
Fornari-Simone <65810491+Fornari-Simone@users.noreply.github.com>
2020-10-14 15:30:10 +0200

    Try push

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 let options = [ "Values", "Data"  , "Lenght", "Temperature", 
                 "Weight", "Volume", "Area"  , "Time"];
-                
 let createDiv = () => {
     options.forEach(element => {
         document.body.innerHTML(
