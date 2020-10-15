7c685b0f1afaea863592c2b9648f731778e310eb
Fornari-Simone <65810491+Fornari-Simone@users.noreply.github.com>
2020-10-15 12:40:52 +0200

    adjust index.js for html

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
 let options = [ "Values", "Data"  , "Lenght", "Temperature", 
                 "Weight", "Volume", "Area"  , "Time"];
-let innHtml = `<h1>Coverter of Candido and Fornari</h1>\n`;
-options.forEach(element => {
-	innHtml += `<div id="${element}">\n
-		<h2>${element}</h2>\n
-		
-		<div>\n
-			<label class="inp" for="${element}">Importo</label>\n
-			<input type="text" name="" class="inp" id="${element}">\n
-		</div>\n
-		<div>\n
-			<label class="inp" for="${element}">Da</label>\n
-			<input type="text" name="" class="inp" id="${element}">\n
-		</div>\n
-		<div>\n
-			<label class="inp" for="${element}">A</label>\n
-			<input type="text" name="" class="inp" id="${element}">\n
-		</div>\n
+let innHtml = `<h1>Coverter of Candido and Fornari</h1>\n
+		<select name="Types" id="type">\n`
+options.forEach(element => innHtml += `<option value="${element}">${element}</option>\n`);
+innHtml += `</select>\n	
+	<div>\n
+		<ul>\n
+			<li><label class="inp" for="importo">Importo</label></li>\n
+			<li><input type="text" name="importo" class="inp"></li>\n
+		</ul>\n
+		<ul>\n
+		<li><label class="inp" for="da">Da</label></li>\n
+		<li><input type="text" name="da" class="inp"></li>\n
+		</ul>\n
+		<img src="img/exchange.png" alt="">
+		<ul>\n
+		<li><label class="inp" for="a">A</label></li>\n
+		<li><input type="text" name="a" class="inp"></li>\n	
+		</ul>\n
 	</div>\n`
-});
 document.getElementById("body").innerHTML = innHtml;
